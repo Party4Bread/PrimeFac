0459c34858f5e62d577238d6ccee6ee9a864f1ff
Party4Bread <solsam10@naver.com>
2018-04-24 20:44:02 +0900

    Add PollardRho of loop limit version

--- PrimeFac/PrimeFac.cs
+++ PrimeFac/PrimeFac.cs
@@ -5,7 +5,7 @@ namespace PrimeFac
 {
     public static class PrimeFac
     {
-        public static BigInteger PollardRho(BigInteger n)//TODO : set cycle limit, figure out how this work
+        public static BigInteger PollardRho(BigInteger n)
         {
             BigInteger x_fixed = 2, cycle_size = 2, x = 2, factor = 1;
 
@@ -21,6 +21,31 @@ namespace PrimeFac
                 x_fixed = x;
             }
             return factor;
+        }
+        public static bool PollardRho(BigInteger n, out BigInteger result, int limit = 100000)
+        {
+            BigInteger x_fixed = 2, cycle_size = 2, x = 2, factor = 1;
+
+            while (factor == 1&& cycle_size<limit)
+            {
+                for (int count = 1; count <= cycle_size && factor <= 1; count++)
+                {
+                    x = (x * x + 1) % n;
+                    factor = Util.GCD(x - x_fixed, n);
+                }
+
+                cycle_size *= 2;
+                x_fixed = x;
+            }
+            if (factor != 1)
+            {
+                result = factor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         public static BigInteger Fermat(BigInteger n)
         {
@@ -47,7 +72,7 @@ namespace PrimeFac
         /// <param name="limit">Loop Limit</param>
         /// <param name="result">Return one of prime</param>
         /// <returns>Return that was successful attack</returns>
-        public static bool Fermat(BigInteger n, out BigInteger result, int limit = 10000)
+        public static bool Fermat(BigInteger n, out BigInteger result, int limit = 100000)
         {
             BigInteger x, y, w;
             Util.isqrt(n, out x);
--- PrimeFacTest/UnitTest1.cs
+++ PrimeFacTest/UnitTest1.cs
@@ -20,21 +20,27 @@ namespace PrimeFac
             BigInteger n = new BigInteger(204791) * 0x10001;
             Assert.AreEqual(0x10001, PrimeFac.PollardRho(n));
         }
-        //This may not end in a day don't try it
-        //[TestMethod]
+        [TestMethod]
         public void PollardRhoTest3()
+        {
+            BigInteger n = new BigInteger(204791) * 0x10001, a;
+            PrimeFac.PollardRho(n, out a);
+            Assert.AreEqual(0x10001, a);
+        }
+        [TestMethod]
+        public void PollardRhoTimeoutTest()
         {
             using (RSA rsa = RSA.Create(512))
             {
                 var key = rsa.ExportParameters(true);
-                BigInteger n = new BigInteger(key.Modulus);
-                Assert.AreEqual(new BigInteger(key.P), PrimeFac.PollardRho(n));
+                BigInteger n = new BigInteger(key.Modulus), a;
+                Assert.AreEqual(false, PrimeFac.PollardRho(n, out a, 1));
             }
         }
         [TestMethod]
         public void FermatTest1()
         {
-            BigInteger n = 97*103;
+            BigInteger n = 97 * 103;
             Assert.AreEqual(97, PrimeFac.Fermat(n));
         }
         [TestMethod]
@@ -46,18 +52,18 @@ namespace PrimeFac
         [TestMethod]
         public void FermatTest3()
         {
-            BigInteger n = new BigInteger(204791) * 0x10001,k;
-            PrimeFac.Fermat(n, out k,100000);
-            Assert.AreEqual(0x10001,k);
+            BigInteger n = new BigInteger(204791) * 0x10001, k;
+            PrimeFac.Fermat(n, out k, 100000);
+            Assert.AreEqual(0x10001, k);
         }
         [TestMethod]
-        public void FermatTestTimeout()
+        public void FermatTimeoutTest()
         {
             using (RSA rsa = RSA.Create(512))
             {
                 var key = rsa.ExportParameters(true);
-                BigInteger n = new BigInteger(key.Modulus),a;
-                Assert.AreEqual(false, PrimeFac.Fermat(n, out a,1));
+                BigInteger n = new BigInteger(key.Modulus), a;
+                Assert.AreEqual(false, PrimeFac.Fermat(n, out a, 1));
             }
         }
     }
